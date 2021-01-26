63da4abfbc64aa4d2bd79ebcd44978f98580d134
홍성민 <honggasm@gmail.com>
2021-01-26 22:17:29 +0900

    feat: 엔티티 생성

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    // 실무에서는 절대 사용하지 말것. 중간 테이블에 추가적으로 컬럼을 추가할 수 없으니 사용성이 아주 떨어진다.
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
