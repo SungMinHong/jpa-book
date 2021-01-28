03319aafed80af1889ebd2995909741c1fcce0a4
홍성민 <honggasm@gmail.com>
2021-01-28 21:55:17 +0900

    feat: 회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+    
+    private final MemberRepository repository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(final Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(final Member member) {
+        final var members = repository.findByName(member.getName());
+        if (!members.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+    
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+    
+    public Member findOne(final Long memberId) {
+        return repository.findOne(memberId);
+    }
+}
