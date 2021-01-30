893d39e4b7a6dd92978a33a1b4ef634078987c80
홍성민 <honggasm@gmail.com>
2021-01-31 00:08:31 +0900

    feat: 주문 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(final Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(final Long id) {
+        return em.find(Order.class, id);
+    }
+    
+    // TODO:: 이름으로 검색 주문 상태로 검색 추후 추가
+}
