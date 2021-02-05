71d5cc63df6ec0ba6d04675d330b1c0765be2db3
홍성민 <honggasm@gmail.com>
2021-02-05 21:06:40 +0900

    feat: 회원조회 API 추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,18 +2,29 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("api/v1/members")
+    public Result<List<MemberDTO>> getMembers() {
+        final var members = memberService.findMembers().stream()
+                .map(member -> new MemberDTO(member.getName()))
+                .collect(Collectors.toList());
+        return new Result<>(members);
+    }
 
     @PostMapping("/api/v1/members")
     public CreatedMemberResponse saveMember(@RequestBody @Valid final CreateMemberRequest request) {
@@ -54,4 +65,17 @@ public class MemberApiController {
         private final Long id;
         private final String name;
     }
+
+    @Getter
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class MemberDTO {
+        private final String name;
+    }
+
 }
