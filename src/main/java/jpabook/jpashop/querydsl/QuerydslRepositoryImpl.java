8aae39680f9bb4ce889bb5c3924eb400a5bd98cd
홍성민 <honggasm@gmail.com>
2021-02-09 20:22:22 +0900

    feat: querydsl offset, limit

--- src/main/java/jpabook/jpashop/querydsl/QuerydslRepositoryImpl.java
+++ src/main/java/jpabook/jpashop/querydsl/QuerydslRepositoryImpl.java
@@ -16,12 +16,15 @@ public class QuerydslRepositoryImpl implements QuerydslRepository{
     @NonNull
     private final JPAQueryFactory queryFactory;
 
+    // 인덱스가 있는 경우 like 검색은 빨라 지는가
     @Override
     public List<Item> findItemsByName(final String name) {
         return queryFactory
                 .select(item)
                 .from(item)
-                .where(item.name.eq(name))
+                .where(item.name.like(name).and(item.price.gt(9999)))
+                .offset(0)
+                .limit(1)
                 .fetch();
     }
 
